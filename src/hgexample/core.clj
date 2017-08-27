aa0bd0ef6f417166d00b3d6f32671a97379259a6
javier <javierarrieropais@outlook.com>
2017-08-27 18:17:16 +0200

    Correct event distribution

--- src/hgexample/core.clj
+++ src/hgexample/core.clj
@@ -10,61 +10,72 @@
         pub (a/pub c :node)]
     {:router c :pub pub}))
 
-(defn get-current-top [hg pk]
-  (-> hg (get pk) first :hash))
+(defn self-parent? [hg-ev
+                    {parent?-hash :hash}
+                    {self-parent :self}]
+  (loop [self-parent-hash self-parent]
+    (or (= parent?-hash self-parent-hash)
+        (when self-parent-hash
+          (recur (get-in hg-ev [self-parent-hash :self]))))))
+
+(defn create-event [pk payload & [self other]]
+  (merge
+    (when self {:self self})
+    (when other {:other other})
+    {:hash  (UUID/randomUUID)
+     :node  pk
+     :tx    payload}))
 
 (defn create-node [config]
   (let [pk (:pk config)
         pub (:pub config)
         router (:router config)
         delay-ms (or (:delay-ms config) 1000)
-        in-ch (a/chan)
+        in-ch (a/chan 10)
         new-ch (a/chan)
-        hg (atom {pk [{:tx [] :hash (UUID/randomUUID) :node pk}]})
+        uuid (UUID/randomUUID)
+        initial-ev (create-event pk [])
+        hg-ev (atom {(:hash initial-ev) initial-ev})
+        hg-heads (atom {pk (:hash initial-ev)})
         nodes (:nodes config)]
     (a/sub pub pk in-ch)
-    (>!! router {:node (rand-nth nodes) :data {:events [(-> (@hg pk) first)] :heads {}}})
+    #_(doseq [nodek nodes]
+      (>!! router {:node nodek :data {:events [(-> (@hg pk) first)] :heads {}}}))
+    ;(synchronize @hg [])
     (go-loop []
       (Thread/sleep (+ 100 (* (rand-int 4) 1000)))
-      (>! new-ch [(str "event from node" pk)])
-      (recur))
-    (go-loop [new (<! new-ch)]
-      (let [that-pk (rand-nth nodes)
+      (when (>! new-ch [(str "event from node" pk)])
+        (recur)))
+    (go-loop []
+      (let [that-pk (rand-nth (remove #(= % pk) nodes))
             [incoming ch] (alts! [in-ch (a/timeout delay-ms)])]
-        (if (and (= ch in-ch) incoming)
-          (let [ev (first (get-in incoming [:data :events]))
-                heads (get-in incoming [:data :heads])
-                nodek (:node ev)
-                event {:hash (UUID/randomUUID)
-                       :node pk
-                       :tx new
-                       :self (get-current-top @hg pk)
-                       :other (:hash ev)}]
-            (swap! hg update nodek #(cons ev %))
-            (swap! hg update pk #(cons event %))
-            (let [this-heads (medley/map-kv
-                               (fn [k v]
-                                 [k (or (-> (map :hash v)
-                                            set
-                                            (contains? (get heads k)))
-                                        false)])
-                               @hg)
-                  that-missing (->> (medley/filter-vals #(= false %) this-heads)
-                                    (keys)
-                                    (select-keys @hg)
-                                    (medley/map-kv (fn [k v]
-                                                     (medley/take-upto #(= (:hash %) (get heads k)) v)))
-                                    (vals)
-                                    (flatten))]
-              (info that-missing)
-              ;(>! router {:node nodek :data {:events event :heads (medley/map-vals first @hg) }})
-              )
-            (>! router {:node that-pk :data {:events [event] :heads (medley/map-vals first @hg)}})
-            (Thread/sleep delay-ms)
-            (recur (<! new-ch))))))
+        (when-let [event
+                   (cond
+                     (and (= ch in-ch) incoming)
+                     (let [ev-hash (get-in incoming [:data :event :hash])
+                           updates (get-in incoming [:data :update])
+                           event (create-event pk (<! new-ch) (@hg-heads pk) ev-hash)]
+                       (doseq [ev updates]
+                         (swap! hg-ev assoc (:hash ev) ev))
+                       (doseq [ev updates
+                               :let [nodek (:node ev)]]
+                         (when (self-parent? @hg-ev (@hg-heads nodek) ev)
+                           (swap! hg-heads assoc nodek (:hash ev))))
+                       (swap! hg-ev assoc (:hash event) event)
+                       (swap! hg-heads assoc pk (:hash event))
+                       event)
+
+                     (not= ch in-ch)
+                     (get @hg-ev (@hg-heads pk))
+
+                     :else nil)]
+          (>! router {:node that-pk :data {:event event :update (vals @hg-ev)}})
+          (Thread/sleep delay-ms)
+          (recur))))
     {:in-ch in-ch
      :new-ch new-ch
-     :hg hg}))
+     :hg-heads hg-heads
+     :hg-ev hg-heads}))
 
 (defn -main
   "I don't do a whole lot ... yet."
